122f84dfbb9f742bc1c64bb5a6e74be62250435c
Diego <android.mxdiego9@gmail.com>
2020-09-27 15:52:33 +0900

    commit

---
+++ Readme.md
@@ -0,0 +1,50 @@
+# pgm converter
+
+Convert and clop pgm image.
+
+## Usage
+
+```
+$ pgm_converter --help
+```
+
+to see help
+
+```
+pgm_converter 0.1.0
+
+USAGE:
+    pgm_converter [OPTIONS] --dst <dst> --mode <mode> --src <src>
+
+FLAGS:
+        --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -m, --mode <mode>        Output PGM Type (b: Binary, a: Ascii)
+    -s, --src <src>          Source file path
+    -d, --dst <dst>          Output file path
+    -l, --left <left>        left corrdinate [default: 0]
+    -t, --top <top>          top cordinate [default: 0]
+    -w, --width <width>      width (Optional)
+    -h, --height <height>    height (Optional)
+```
+
+## Example
+- Convert `./example.pgm` into Ascii.
+
+    ```shell
+    $ pgm_converter -m a -s ./example.pgm -d ./example_cloped_ascii.pgm
+    ```
+
+-  Convert `./example.pgm` into Ascii and clop (3,3).
+
+    ```shell
+    $ pgm_converter -m a -s ./example.pgm -d ./example_cloped_ascii.pgm -l 3 -t 3
+    ```
+
+-  Convert `./example.pgm` into Ascii and clop (3,3)-(9,9).
+
+    ```shell
+    $ pgm_converter -m a -s ./example.pgm -d ./example_cloped_ascii.pgm -l 3 -t 3 -w 6 -h 6
+    ```
---
+++ src/main.rs
@@ -0,0 +1,154 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufWriter},
+    path::PathBuf,
+    str::FromStr,
+};
+use structopt::StructOpt;
+use structopt::clap::{AppSettings::DeriveDisplayOrder};
+#[derive(StructOpt, Debug)]
+#[structopt(name = "pgm_converter", setting = DeriveDisplayOrder )]
+struct CliOption {
+    /// Output PGM Type (b: Binary, a: Ascii)
+    #[structopt(short, long)]
+    mode: FileType,
+    /// Source file path
+    #[structopt(short, long, parse(from_os_str))]
+    src: PathBuf,
+    /// Output file path
+    #[structopt(short, long, parse(from_os_str))]
+    dst: PathBuf,
+
+    /// left corrdinate
+    #[structopt(short, long, default_value = "0")]
+    left: u32,
+    /// top cordinate
+    #[structopt(short, long, default_value = "0")]
+    top: u32,
+
+    /// width (Optional)
+    #[structopt(short, long)]
+    width: Option<u32>,
+    /// height (Optional)
+    #[structopt(short, long)]
+    height: Option<u32>,
+}
+
+#[derive(Debug)]
+enum FileType {
+    Binary,
+    Ascii,
+}
+impl FromStr for FileType {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "b" | "binary" | "Binary" => Ok(FileType::Binary),
+            "a" | "ascii" | "Ascii" => Ok(FileType::Ascii),
+            _ => Err(
+                r#"Unknown type. Use "b" or "Binary" to P5 File, "a" or "Ascii" to P2 File."#
+                    .to_string(),
+            ),
+        }
+    }
+}
+fn main() {
+    let opt: CliOption = CliOption::from_args();
+    match doit(opt) {
+        Ok(v) => println!("{}", v),
+        Err(v) => println!("Error: {}", v),
+    }
+}
+fn doit(opt: CliOption) -> Result<String, String> {
+    let img = match {
+        match image::open(opt.src.clone()) {
+            Ok(v) => v,
+            Err(_) => {
+                return Err(format!(
+                    "source img not found: {}",
+                    opt.src.to_str().unwrap()
+                ))
+            }
+        }
+        .as_luma8()
+    } {
+        Some(v) => v,
+        None => {
+            return Err("sorce file is not 8bit 
+        Monochrome"
+                .to_string())
+        }
+    }
+    .to_owned();
+
+    let (img_width, img_height) = img.dimensions();
+
+    let (x_range, y_range) = {
+        let left = opt.left;
+        let top = opt.top;
+        let right = if let Some(width) = opt.width {
+            let row_right = width + left;
+            if row_right > img_width {
+                img_width
+            } else {
+                row_right
+            }
+        } else {
+            img_width
+        };
+        let bottom = if let Some(height) = opt.height {
+            let row_bottom = height + left;
+            if row_bottom > img_height {
+                img_height
+            } else {
+                row_bottom
+            }
+        } else {
+            img_height
+        };
+        (left..right, top..bottom)
+    };
+
+    let mut writer = BufWriter::new(File::create(opt.dst).unwrap());
+
+    let max = x_range
+        .clone()
+        .clone()
+        .zip(y_range.clone())
+        .map(|(x, y)| img.get_pixel(x, y).0[0])
+        .max()
+        .unwrap();
+    let magic_number = match opt.mode {
+        FileType::Binary => "P5",
+        FileType::Ascii => "P2",
+    };
+    let header = format!(
+        "{}\n{} {}\n{}\n",
+        magic_number,
+        x_range.len(),
+        y_range.len(),
+        max
+    );
+    writer.write(header.as_bytes()).unwrap();
+    match opt.mode {
+        FileType::Ascii => {
+            for y in y_range {
+                let line = (x_range.clone())
+                    .map(|x| format!("{}", img.get_pixel(x, y).0[0]))
+                    .collect::<Vec<_>>()
+                    .join(" ");
+                let line_lf = line + "\n";
+                writer.write(line_lf.as_bytes()).unwrap();
+            }
+        }
+        FileType::Binary => {
+            for y in y_range {
+                for x in x_range.clone() {
+                    writer.write(&img.get_pixel(x, y).0).unwrap();
+                }
+            }
+        }
+    }
+    Ok("finish".to_string())
+}
